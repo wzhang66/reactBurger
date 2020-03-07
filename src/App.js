03fce9035d8927893fb78bf13bd7b25fd92085d7
Weiwei <ww.zhang@mail.utoronto.ca>
2020-03-07 18:01:18 -0500

    building the layout and the basic ingredient component of this app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello world</h1>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
